91a9aa778b479e6b83d555e6a6c10aede9fbc3ba
yoshida444 <yoshida.race@gmail.com>
2025-09-24 01:22:09 +0700

    feat: add missing file

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,24 @@
+import { utils } from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
+
 export const DEFAULT_SLIPPAGE = "0.1"; // 0.1%
+
+// SPL Token program ids (classic + 2022)
+export const TOKEN_PROGRAM_ID = new PublicKey(
+  "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
+);
+export const TOKEN_2022_PROGRAM_ID = new PublicKey(
+  "TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb",
+);
+
+// bytes code for amm instruction seeds
+export const POOL_LPMINT_SEED = Buffer.from(
+  utils.bytes.utf8.encode("pool_lp_mint"),
+);
+export const POOL_VAULT_SEED = Buffer.from(
+  utils.bytes.utf8.encode("pool_vault"),
+);
+export const POOL_SEED = Buffer.from(utils.bytes.utf8.encode("pool"));
+export const POOL_AUTH_SEED = Buffer.from(
+  utils.bytes.utf8.encode("vault_and_lp_mint_auth_seed"),
+);
---
+++ lib/utils/instructions.ts
@@ -0,0 +1,56 @@
+import { PublicKey } from "@solana/web3.js";
+import {
+  POOL_AUTH_SEED,
+  POOL_LPMINT_SEED,
+  POOL_SEED,
+  POOL_VAULT_SEED,
+} from "@/lib/constants";
+
+export function getPoolAddress(
+  ammConfig: PublicKey,
+  tokenMint0: PublicKey,
+  tokenMint1: PublicKey,
+  programId: PublicKey,
+): [PublicKey, number] {
+  const [address, bump] = PublicKey.findProgramAddressSync(
+    [
+      POOL_SEED,
+      ammConfig.toBuffer(),
+      tokenMint0.toBuffer(),
+      tokenMint1.toBuffer(),
+    ],
+    programId,
+  );
+  return [address, bump];
+}
+
+export function getPoolLpMintAddress(
+  pool: PublicKey,
+  programId: PublicKey,
+): [PublicKey, number] {
+  const [address, bump] = PublicKey.findProgramAddressSync(
+    [POOL_LPMINT_SEED, pool.toBuffer()],
+    programId,
+  );
+  return [address, bump];
+}
+
+export function getPoolVaultAddress(
+  pool: PublicKey,
+  vaultTokenMint: PublicKey,
+  programId: PublicKey,
+): [PublicKey, number] {
+  const [address, bump] = PublicKey.findProgramAddressSync(
+    [POOL_VAULT_SEED, pool.toBuffer(), vaultTokenMint.toBuffer()],
+    programId,
+  );
+  return [address, bump];
+}
+
+export function getAuthAddress(programId: PublicKey): [PublicKey, number] {
+  const [address, bump] = PublicKey.findProgramAddressSync(
+    [POOL_AUTH_SEED],
+    programId,
+  );
+  return [address, bump];
+}
